979b64b702c13f4f9387e49ccebe3245f8b6997f
lexborelli <alexasborelli@gmail.com>
2023-09-05 13:56:47 -0400

    fixed typing error

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const submitBtn = document.querySelector(".submit-btn");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message"); 
+const subjectInput = document.querySelector("#subject");
 
 const publicKey = "VgQq5a7HAxfmx1TS_";
 const serviceID = "service_4aomxmt";
@@ -16,7 +17,8 @@ contactForm.addEventListener("submit", e => {
     const inputFields = {
         name: nameInput.value,
         email: emailInput.value,
-        message: messageInput.value
+        message: messageInput.value,
+        subject: subjectInput.value
     }
 
     emailjs.send(serviceID, templateID, inputFields)
@@ -25,7 +27,10 @@ contactForm.addEventListener("submit", e => {
         nameInput.value = "";
         emailInput.value = "";
         messageInput.value = "";
-    }, (error) => {
+        subjectInput.value = "";
+    }, 
+    
+    (error) => {
         console.log(error);
         
         submitBtn.innerText = "Something went wrong";
